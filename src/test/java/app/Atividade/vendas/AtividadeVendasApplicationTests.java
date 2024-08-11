012c8656a2b1e7ed3c8dfcff4bd2cd54a477516d
Kaila <kailacamila15@gmail.com>
2024-08-11 14:44:34 -0300

    entity feita

---
+++ src/test/java/app/Atividade/vendas/AtividadeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package app.Atividade.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
